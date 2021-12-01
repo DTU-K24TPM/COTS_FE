9960b805b99475b4ca59b55ebf0b60a5bdbfd260
Dieu Ny <dieuny2603@gmail.com>
2021-12-01 15:38:57 +0700

    sửa and chi tiết ạ

--- js/modal.js
+++ js/modal.js
@@ -1,31 +1,24 @@
 var modal = document.getElementById('myModal');
+var modalchitiet = document.getElementById('chitietModal');
     var btn = document.getElementById("myBtn");
-    var span = document.getElementsByClassName("close")[0];
+    var btnchitiet = document.getElementById("chitietBtn");
+    var span = document.getElementsByClassName("suaclose")[0];
+    var spanchitiet = document.getElementsByClassName("chitietclose")[0];
     btn.onclick = function () {
         modal.style.display = "block";
     }
+    btnchitiet.onclick = function () {
+        modalchitiet.style.display = "block";
+    }
     span.onclick = function () {
         modal.style.display = "none";
+    }
+    spanchitiet.onclick = function () {
+        modalchitiet.style.display = "none";
     }
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     }
-
-    var modal = document.getElementById('myModal1');
-    var btn = document.getElementById("myBtn1");
-    var span = document.getElementsByClassName("close1")[0];
-    btn.onclick = function () {
-        modal.style.display = "block";
-    }
-
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+   
